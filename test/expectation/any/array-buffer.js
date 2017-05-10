6ded7781cb9190a83388c6ce43146e62fca4fd44
chrisguttandin <chrisguttandin@media-codings.com>
2017-05-10 14:33:41 +0200

    simplify expectation test

--- test/expectation/any/array-buffer.js
+++ test/expectation/any/array-buffer.js
@@ -2,80 +2,26 @@ describe('ArrayBuffer', () => {
 
     describe('constructor()', () => {
 
-        let millisecondsPerFrame;
         let length;
 
-        beforeEach(function (done) {
-            this.timeout(5000);
-
+        beforeEach(() => {
             length = 2147479551;
-
-            // Wait some time to allow the browser to warm up.
-            setTimeout(() => {
-                const numberOfCycles = 50;
-
-                let remainingCycles = numberOfCycles;
-                let timeAtFirstCycle;
-
-                const cycle = () => {
-                    if (remainingCycles === numberOfCycles) {
-                        timeAtFirstCycle = performance.now();
-                    }
-
-                    if (remainingCycles === 0) {
-                        millisecondsPerFrame = (performance.now() - timeAtFirstCycle) / numberOfCycles;
-
-                        done();
-                    } else {
-                        remainingCycles -= 1;
-
-                        requestAnimationFrame(() => cycle());
-                    }
-                };
-
-                requestAnimationFrame(() => cycle());
-            }, 1000);
         });
 
-        it('should block the main thread', function (done) {
+        it('should block the main thread', function () {
             this.timeout(5000);
 
-            let arrayBuffer; // eslint-disable-line no-unused-vars
-            let remainingMinimalCycles = 10;
-            let timeAtLastCycle = null;
+            let lastNow = performance.now();
+            let currentNow = performance.now();
 
-            const cycle = () => {
-                const now = performance.now();
+            expect(currentNow - lastNow).to.be.below(100);
 
-                try {
-                    if (timeAtLastCycle !== null) {
-                        const elapsedTime = now - timeAtLastCycle;
+            new ArrayBuffer(length);
 
-                        if (remainingMinimalCycles === 7) {
-                            expect(elapsedTime).to.be.above(millisecondsPerFrame / 10);
-                        } else {
-                            expect(elapsedTime).to.be.below(millisecondsPerFrame * 10);
-                        }
+            lastNow = currentNow;
+            currentNow = performance.now();
 
-                        remainingMinimalCycles -= 1;
-
-                        if (remainingMinimalCycles === 7) {
-                            // Keep a reference until the test is over to avoid garbage collection.
-                            arrayBuffer = new ArrayBuffer(length);
-                        } else if (remainingMinimalCycles === 0) {
-                            done();
-                        }
-                    }
-
-                    timeAtLastCycle = now;
-
-                    requestAnimationFrame(() => cycle());
-                } catch (err) {
-                    done(err);
-                }
-            };
-
-            requestAnimationFrame(() => cycle());
+            expect(currentNow - lastNow).to.be.above(100);
         });
 
     });
